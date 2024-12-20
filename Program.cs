ca6ea00d685ff124118974cd0dbfe53e62466c76
madhu-30123 <2200030123@kluniversity.in>
2024-12-20 14:13:51 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program{
+    static void Main(){
+        Console.WriteLine("Enter two numbers");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Sum:" + (num1 + num2));
+        Console.WriteLine("Difference:" + (num1 - num2));
+        Console.WriteLine("product :2" + (num1*num2));
+        Console.WriteLine("Division:" + (num1 / num2));
+    }
+}
